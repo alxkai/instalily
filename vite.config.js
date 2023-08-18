dc1baf8e0b4ec502339930b937b30b3b28da3115
alxkco <137307571+alxkai@users.noreply.github.com>
2023-08-17 22:53:43 -0400

    set base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const root = resolve(__dirname, 'src') // set directory
-const outDir = resolve(__dirname, 'dist')
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
   base: "/instalily/"
 })
