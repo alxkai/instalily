d683c50dee9166165d754720bfdee12cf15f3c09
alxkco <137307571+alxkai@users.noreply.github.com>
2023-08-17 23:19:41 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+const root = resolve(__dirname, 'src') // set directory
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
   plugins: [react()],
-  base: "/instalily"
+  base: "/instalily",
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+
+      }
+    }
+  }
 })
