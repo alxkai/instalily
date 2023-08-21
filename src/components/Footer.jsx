b87833889b77a58c55e656f9f1fc92688a237929
alxkco <137307571+alxkai@users.noreply.github.com>
2023-08-21 07:48:21 -0400

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 mt-[-20px] w-full`}>
       <div className="flex-1 flex flex-col justify-start mr-10">
         <img 
           src={logo}
@@ -21,8 +21,8 @@ const Footer = () => (
 
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerLink) => (
-          <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-[#181818]">
+          <div key={footerLink.key} className="flex flex-col ss:my-0 my-8 min-w-[150px]">
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-[#181818] mt-5">
               {footerLink.title}
             </h4>
             <ul className="list-none mt-4">
--- src/components/How0.jsx
+++ src/components/How0.jsx
@@ -1,33 +1,62 @@
+import React from 'react';
+import { motion } from 'framer-motion';
 import styles from '../style';
-import { how, arrowDown, hero } from '../assets';
+import { how, arrowDown } from '../assets';
 
 const How0 = () => (
-  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-   <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-     <div className="flex flex-row mb-3.5 justify-between items-center w-full">
-       <h1 className="flex-1 font-inter font-semibold ss:text-[32px] text-[32px] text-[#181818] ss:leading-[140%] leading-[140%]">
+  <motion.section
+    id="home"
+    className={`flex md:flex-row flex-col ${styles.paddingY}`}
+  >
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="flex flex-row mb-3.5 justify-between items-center w-full"
+      >
+        <h1 className="flex-1 font-inter font-semibold ss:text-[32px] text-[32px] text-[#181818] ss:leading-[140%] leading-[140%]">
           How It Works
           <img src={arrowDown} alt="arrowDown" className="w-[28px] h-[28px] ml-2 mb-1 inline" />
         </h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1 }}
+        className="flex flex-row justify-between items-center w-full"
+      >
+        <h1 className="flex-1 font-inter font-semibold ss:text-[65px] text-[65px] text-[#181818] ss:leading-[125%] leading-[125%]">
+          <span className="text-gradient">Take Action to</span> {" "}
+          <em className="font-bold">Turbocharge</em> {" "}
+          <span className="text-gradient">Growth.</span> {" "}
+        </h1>
+      </motion.div>
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1.5 }}
+        className={`${styles.paragraph} max-w-[470px] mt-5 mb-4`}
+      >
+        Instalily provides you with a personalized AI Agent that saves you both time and money - automate tasks and increase your output 3-4x.
+      </motion.p>
     </div>
-     <div className="flex flex-row justify-between items-center w-full">
-       <h1 className="flex-1 font-inter font-semibold ss:text-[65px] text-[65px] text-[#181818] ss:leading-[125%] leading-[125%]">
-         <span className="text-gradient" >Take Action to</span> {" "}
-         <em className="font-bold">Turbocharge</em> {" "}
-         <span className="text-gradient" >Growth.</span> {" "}
-       </h1>
-     </div>
-     <p className={`${styles.paragraph} max-w-[470px] mt-5 mb-4`}> Instalily provides you with a personalized AI Agent that saves you both time and money - automate tasks and increase your output 3-4x.</p>
-   </div>
 
-   <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-     <img src={how} alt="billing" className="max-w-[450px] h-auto relative z-[5] mr-10" />
-   </div>
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <motion.img
+        src={how}
+        alt="billing"
+        className="max-w-[450px] h-auto relative z-[5] mr-10"
+        initial={{ x: 1000 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1.5, delay: 1 }}
+      />
+    </div>
 
-   <div className={`ss:hidden ${styles.flexCenter}`}>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      {/* Other content */}
+    </div>
+  </motion.section>
+);
 
-   </div>
-  </section>
-)
-
-export default How0
+export default How0;
